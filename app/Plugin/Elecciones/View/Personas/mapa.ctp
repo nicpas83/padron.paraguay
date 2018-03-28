86840cc914d7987268236769698f0d2f7d763e06
nicpas <nicpas@gmail.com>
2018-03-28 16:44:08 -0300

    adss

--- app/Plugin/Elecciones/View/Personas/mapa.ctp
+++ app/Plugin/Elecciones/View/Personas/mapa.ctp
@@ -1,8 +1,9 @@
-<?php 
-
+<?php
+$this->Html->script("https://maps.googleapis.com/maps/api/js?key=" . AppConfig::get('site.googlemaps.key'), array('inline' => false));
 $this->Html->script('personas/mapa_personas', array('inline' => false));
-
 ?>
 <div class="row mt15">
-    <div id="Mapa" class="col-xl-6 col-12 " style="height: 350px;"></div>
+    <div class="col-12">
+        <div id="mapaPersonas" style="width: 100%; height: 600px;"></div>
+    </div>
 </div>
--- app/webroot/js/personas/mapa_personas.js
+++ app/webroot/js/personas/mapa_personas.js
@@ -1,40 +1,23 @@
-var map, geocoder;
+var map;
 var inscriptoMarker, markers = [];
 
-function mapa_personas_init() {
-
-    map = new google.maps.Map(document.getElementById(field.attr("id") + "Mapa"), {
+$(document).ready(function () {
+    map = new google.maps.Map(document.getElementById("mapaPersonas"), {
         center: {lat: -34.61, lng: -58.45},
         zoom: 11
     });
     $.get(WWW + "elecciones/personas/ajax_get_personas/", function (data) {
         var jdata = $.parseJSON(data);
-        console.log(jdata);
         for (var i in jdata) {
-            var markerColor;
-            
-            var position = jdata[i].Persona.location.split(",");
+            var position = jdata[i].PersonaUbicacion.location.split(",");
             if (position.length == 2) {
-
                 var marker = new google.maps.Marker({
                     position: {lat: parseFloat(position[0]), lng: parseFloat(position[1])},
                     map: map,
-                    id: jdata[i].Aula.id,
-                    title: jdata[i].Aula.nombre,
-                    icon: markerColor
+                    icon: 'https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0'
                 });
-                
                 markers.push(marker);
             }
-
         }
     });
-
-    
-}
-
-$(document).ready(function () {
-    
-    mapa_personas_init();
-    
 });
